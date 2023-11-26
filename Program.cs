86de15b7887694662943f5edd982a871b0a02597
Nick <nick@prpel.co.uk>
2023-11-26 10:27:43 +0000

    initial run

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Buffers;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Running;
+using Microsoft.CodeAnalysis;
+
+BenchmarkRunner.Run<SearchBenchmark>();
+
+[MemoryDiagnoser(false)]
+[SimpleJob(RunStrategy.Throughput, iterationCount: 5)]
+public class SearchBenchmark
+{
+    private static readonly string base64Characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    private static readonly SearchValues<char> Base64SearchValues = SearchValues.Create(base64Characters);
+
+    private static readonly HashSet<char> Base64HashSet = new HashSet<char>(base64Characters);
+
+    [Params("asdioeoecnEFeciecqpoEJ", "asdioeoecnEF^eciecqpoEJ")]
+    public string ExampleText { get; set; }
+
+    [Benchmark]
+    public bool IsBase64_WithChars()
+    {
+        return ExampleText.All(base64Characters.Contains);
+    }
+
+    [Benchmark]
+    public bool IsBase64_WithHashSet()
+    {
+        return ExampleText.All(Base64HashSet.Contains);
+    }
+
+    [Benchmark]
+    public bool IsBase64_WithSpan()
+    {
+        return ExampleText.AsSpan().IndexOfAnyExcept(base64Characters) == -1;
+    }
+
+    [Benchmark]
+    public bool IsBase64_WithSeachValues()
+    {
+        return ExampleText.AsSpan().IndexOfAnyExcept(Base64SearchValues) == -1;
+    }
+
+}
+
